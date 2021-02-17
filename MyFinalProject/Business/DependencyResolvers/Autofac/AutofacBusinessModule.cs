c6107f108861f68025cc4209813bf0ee5175bf84
Ahmet Berkay Yılmaz <ahmet_66_26@hotmail.com>
2021-02-18 00:12:29 +0300

    Codes
    Add FluentValidtaion and Autofac AOP Techinc . . .

---
+++ MyFinalProject/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+        }
+    }
+}
---
+++ MyFinalProject/Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.ProductName).MaximumLength(2);
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
+            RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürünler 'A' Harfi İle Başlamalı!");
+
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
