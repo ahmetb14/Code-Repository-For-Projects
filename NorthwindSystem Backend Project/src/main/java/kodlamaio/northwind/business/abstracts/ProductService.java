1a410fecd84204f05ce96b2f64a118807867a8a6
Ahmet Berkay Yılmaz <ahmet_66_26@hotmail.com>
2021-05-23 00:46:18 +0300

    Codes
    + Added new Java lesson workspace . . .

--- NorthwindSystem Backend Project/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ NorthwindSystem Backend Project/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -2,10 +2,14 @@ package kodlamaio.northwind.business.abstracts;
 
 import java.util.List;
 
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
 
 public interface ProductService {
-	
-	List<Product> getAll();
-	
+
+	DataResult<List<Product>> getAll();
+
+	Result add(Product product);
+
 }
---
+++ NorthwindSystem Backend Project/src/main/java/kodlamaio/northwind/core/utilities/results/DataResult.java
@@ -0,0 +1,27 @@
+package kodlamaio.northwind.core.utilities.results;
+
+public class DataResult<T> extends Result {
+
+	private T data;
+
+	public DataResult(T data, boolean success, String message) {
+
+		super(success, message);
+		this.data = data;
+
+	}
+
+	public DataResult(T data, boolean success) {
+
+		super(success);
+		this.data = data;
+
+	}
+
+	public T getData() {
+
+		return this.data;
+
+	}
+
+}
