c6d6f3e0470031b5b00cb9dbf014542b150985de
Ahmet Berkay Yılmaz <ahmet_66_26@hotmail.com>
2021-06-06 00:57:43 +0300

    Codes
    +Added new project task . . .

--- NorthwindSystem Backend Project/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ NorthwindSystem Backend Project/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -3,6 +3,7 @@ package kodlamaio.northwind.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin
 public class ProductsController {
 
 	private ProductService productService;
