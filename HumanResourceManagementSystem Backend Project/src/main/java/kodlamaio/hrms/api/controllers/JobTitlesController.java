e7b4ca8a863c40bf9b209110393af40b039bef66
Ahmet Berkay Yılmaz <ahmet_66_26@hotmail.com>
2021-06-08 00:18:12 +0300

    Codes
    + Added crossorigin . . .

--- HumanResourceManagementSystem Backend Project/src/main/java/kodlamaio/hrms/api/controllers/JobTitlesController.java
+++ HumanResourceManagementSystem Backend Project/src/main/java/kodlamaio/hrms/api/controllers/JobTitlesController.java
@@ -2,6 +2,7 @@ package kodlamaio.hrms.api.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import kodlamaio.hrms.entities.concretes.JobTitle;
 
 @RestController
 @RequestMapping("/api/jobtitle")
+@CrossOrigin
 public class JobTitlesController {
 
 	private JobTitleService jobTitleService;
