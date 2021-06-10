a05f91f144c3553c02c1aa97221c9467868a5294
Ahmet Berkay Yılmaz <ahmet_66_26@hotmail.com>
2021-06-10 12:31:32 +0300

    Codes
    + Refactoring project . . .

--- NorthwindSystem Frontend Project/src/pages/ProductDetail.jsx
+++ NorthwindSystem Frontend Project/src/pages/ProductDetail.jsx
@@ -6,43 +6,46 @@ import ProductService from "../services/productService";
 export default function ProductDetail() {
   let { name } = useParams();
 
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState([]);
 
   useEffect(() => {
     let productService = new ProductService();
     productService
       .getByProductName(name)
-      .then((result) => setProduct(result.data.data));
+      .then((result) => setProduct([result.data.data]));
   }, []);
 
   return (
     <div>
-      <Card.Group>
-        <Card fluid>
-          <Card.Content>
-            <Image
-              floated="right"
-              size="mini"
-              src="/images/avatar/large/steve.jpg"
-            />
-            <Card.Header>{product.productName}</Card.Header>
-            <Card.Meta>{product.category?.categoryName}</Card.Meta>
-            <Card.Description>
-              Steve wants to add you to the group <strong>best friends</strong>
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <div className="ui two buttons">
-              <Button basic color="green">
-                Approve
-              </Button>
-              <Button basic color="red">
-                Decline
-              </Button>
-            </div>
-          </Card.Content>
-        </Card>
-      </Card.Group>
+      {product.map((data) => (
+        <Card.Group key="oneproduct">
+          <Card fluid>
+            <Card.Content>
+              <Image
+                floated="right"
+                size="mini"
+                src="/images/avatar/large/steve.jpg"
+              />
+              <Card.Header>{data.productName}</Card.Header>
+              <Card.Meta>{data.category.categoryName}</Card.Meta>
+              <Card.Description>
+                Steve wants to add you to the group{" "}
+                <strong>best friends</strong>
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <div className="ui two buttons">
+                <Button basic color="green">
+                  Approve
+                </Button>
+                <Button basic color="red">
+                  Decline
+                </Button>
+              </div>
+            </Card.Content>
+          </Card>
+        </Card.Group>
+      ))}
     </div>
   );
 }
