a9277e4106c177e7c3572dfe82b2cbd642e11537
Ahmet Berkay Yılmaz <ahmet_66_26@hotmail.com>
2021-06-23 23:22:43 +0300

    Codes
    + Added new project task : formik yup install the project and working . . .

--- NorthwindSystem Frontend Project/src/layouts/Dashboard.jsx
+++ NorthwindSystem Frontend Project/src/layouts/Dashboard.jsx
@@ -6,6 +6,7 @@ import { Route } from "react-router";
 import ProductDetail from "../pages/ProductDetail";
 import CartDetail from "../pages/CartDetail";
 import { ToastContainer } from "react-toastify";
+import ProductAdd from "../pages/ProductAdd";
 
 export default function Dashboard() {
   return (
@@ -21,6 +22,7 @@ export default function Dashboard() {
             <Route exact path="/products" component={ProductList} />
             <Route path="/products/:name" component={ProductDetail} />
             <Route path="/cart" component={CartDetail} />
+            <Route path="/product/add" component={ProductAdd} />
           </Grid.Column>
         </Grid.Row>
       </Grid>
---
+++ NorthwindSystem Frontend Project/src/pages/ProductAdd.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { FormField, Button, Label } from "semantic-ui-react";
+import KodlamaIoTextInput from "../utilities/customFormControls/KodlamaIoTextInput";
+
+export default function ProductAdd() {
+  const initialValues = { productName: "", unitPrice: 10 };
+
+  const schema = Yup.object({
+    productName: Yup.string().required("Ürün adı zorunlu"),
+    unitPrice: Yup.number().required("Ürün fiyatı zorunlu"),
+  });
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={schema}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      <Form className="ui form">
+        <KodlamaIoTextInput name="productName" placeholder="Ürün adı" />
+        <KodlamaIoTextInput name="unitPrice" placeholder="Ürün fiyatı" />
+        <Button color="green" type="submit">
+          Ekle
+        </Button>
+      </Form>
+    </Formik>
+  );
+}
