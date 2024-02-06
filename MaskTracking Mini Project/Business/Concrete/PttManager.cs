298da3211cc0d6b2bf0742599cd0fe02a86259a0
Ahmet Berkay Yılmaz <ahmet_66_26@hotmail.com>
2024-02-06 05:57:29 +0300

    Codes
    Update - new project on repository . . .

---
+++ MaskTracking Mini Project/Business/Concrete/PttManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        private IApplicantService _applicantService;
+
+        public PttManager(IApplicantService applicantService) //Constructor new yapıldığında çalışır
+        {
+            _applicantService = applicantService;
+        }
+        public void GiveMask(Person person)
+        {
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FristName + " için maske VERİLDİ!");
+            }
+            else
+            {
+                Console.WriteLine(person.FristName + " için maske VERİLEMEDİ!");
+            }
+        }
+    }
+}
---
+++ MaskTracking Mini Project/Workspace/Program.cs
@@ -0,0 +1,118 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Degiskenler();
+            Vatandas vatandas1 = new Vatandas();
+
+            SelamVer(isim: "Ahmet");
+            SelamVer(isim: "Berkay");
+            SelamVer(isim: "Yılmaz");
+            SelamVer();
+
+            int sonuc = Topla(6, 58);
+
+            //Diziler : Arrays
+
+            string ogrenci1 = "Ahmet";
+            string ogrenci2 = "Burak";
+            string ogrenci3 = "Tuğra";
+
+            //Console.WriteLine(ogrenci1);
+            //Console.WriteLine(ogrenci2);
+            //Console.WriteLine(ogrenci3);
+
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Ahmet";
+            ogrenciler[1] = "Burak";
+            ogrenciler[2] = "Tuğra";
+
+            ogrenciler = new string[4];
+            ogrenciler[3] = "Mustafa";
+
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            string[] sehirler1 = new[] { "Ankara", "İstanbul", "İzmir" };
+            string[] sehirler2 = new[] { "Bursa", "Antalya", "Diyarbakır" };
+
+            sehirler2 = sehirler1;
+            sehirler1[0] = "Adana";
+            Console.WriteLine(sehirler2[0]);
+
+            Person person1 = new Person();
+            person1.FristName = "AHMET BERKAY";
+            person1.LastName = "YILMAZ";
+            person1.DateOfBrithYear = 2002;
+            person1.NationalIdentity = 123;
+
+            Person person2 = new Person();
+            person2.FristName = "Burak";
+
+            foreach (string sehir in sehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+            //MyList
+            List<string> yeniSehirler1 = new List<string> { "Ankara 1", "İstanbul 1", "İzmir 1" };
+            yeniSehirler1.Add(item: "Adana 1");
+            foreach (var sehir in yeniSehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            Console.ReadLine();
+        }
+
+        static void SelamVer(string isim = "İsimsiz")
+        {
+            Console.WriteLine("Merhaba " + isim);
+        }
+
+        static int Topla(int sayi1 = 5, int sayi2 = 10)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam: " + sonuc.ToString());
+            return sonuc;
+        }
+
+        private static void Degiskenler()
+        {
+            string mesaj = "Merhaba";
+            double tutar = 100000; //db den gelir
+            int sayi = 100;
+            bool girisYapmisMi = false;
+
+            string ad = "Ahmet Berkay";
+            string soyad = "Yılmaz";
+            int dogumYili = 2002;
+            long tcNo = 12345678910;
+
+            Console.WriteLine(tutar * 1.18);
+
+            Console.WriteLine(tutar * 1.18);
+        }
+    }
+
+    //pascal casing
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+}
